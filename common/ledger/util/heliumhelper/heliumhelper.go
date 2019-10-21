14c1253cf4fe38e11d792e1be09ed423f42d446b
Arash Nabili <arash@levyx.com>
2019-10-21 11:02:52 -0700

    Added first version of Helium state db integration

---
+++ common/ledger/util/heliumhelper/heliumhelper.go
@@ -0,0 +1,228 @@
+package heliumhelper
+
+// #cgo LDFLAGS: -lhe
+// #include <stdlib.h>
+// #include <he.h>
+import "C"
+import (
+	"bytes"
+	"errors"
+
+	"github.com/hyperledger/fabric/common/flogging"
+)
+
+const HE_MAX_VAL_LEN = 16777215
+
+const HE_O_CREATE = 1
+const HE_O_TRUNCATE = 2
+const HE_O_VOLUME_CREATE = 4
+const HE_O_VOLUME_TRUNCATE = 8
+
+var logger = flogging.MustGetLogger("heliumhelper")
+
+type Provider struct {
+	heUrl string
+}
+
+func NewProvider(heUrl string) *Provider {
+	return &Provider{heUrl}
+}
+
+func (p *Provider) GetDSHandle(dbName string) *HeDatastore {
+	logger.Debugf("GetDSHandle called; dbName: %s", dbName)
+	// Open a separate datastore for each db
+	c_url := C.CString(p.heUrl)
+	c_name := C.CString(dbName)
+	c_flags := C.int(HE_O_CREATE | HE_O_VOLUME_CREATE) // TODO: Put this in configs
+
+	defer C.free(unsafe.Pointer(c_url))
+	defer C.free(unsafe.Pointer(c_name))
+
+	handle := C.he_open(c_url, c_name, c_flags, nil) // TODO: Get env from configs
+
+	return &HeDatastore{handle}
+}
+
+type HeDatastore struct {
+	handle C.he_t
+}
+
+func (ds *HeDatastore) Put(key []byte, val []byte) error {
+	logger.Debugf("ds.Put called; key size: %d, val size: %d", len(key), len(val))
+	c_key := C.CBytes(key)
+	c_val := C.CBytes(val)
+	key_len := C.size_t(len(key))
+	val_len := C.size_t(len(val))
+
+	defer C.free(c_key)
+	defer C.free(c_val)
+
+	item := C.struct_he_item{c_key, c_val, key_len, val_len}
+
+	ret := C.he_update(ds.handle, &item)
+
+	if int(ret) != 0 {
+		return errors.New("Failed to put item")
+	}
+
+	return nil
+}
+
+func (ds *HeDatastore) Get(key []byte) ([]byte, error) {
+	logger.Debugf("ds.Get called; key size: %d", len(key))
+	c_key := C.CBytes(key)
+	key_len := C.size_t(len(key))
+
+	defer C.free (c_key)
+
+	// Use an initial guess for the val_len
+	val_len := C.size_t(2048) // TODO: Move initial buffer size to config
+	c_val := C.malloc(val_len)
+
+	item := C.struct_he_item{key: c_key, val: c_val, key_len: key_len}
+
+	C.he_lookup(ds.handle, &item, C.size_t(0), val_len)
+
+	if int(item.val_len) > int(val_len) {
+		// Item value size was larger than initial buffer size; reallocate buffer and retry
+		logger.Infof("Initial val_len guess was too small: %d < %d", int(val_len), int(item.val_len))
+		C.free(c_val)
+		val_len = item.val_len
+		c_val = C.malloc(val_len)
+
+		item.val = c_val
+
+		C.he_lookup(ds.handle, &item, C.size_t(0), item.val_len)
+	}
+
+	val_len = item.val_len
+
+	val := C.GoBytes(c_val, C.int(val_len))
+
+	C.free(c_val)
+
+	return val, nil
+}
+
+func (ds *HeDatastore) Close() {
+	logger.Debugf("ds.Close called")
+	C.he_close(ds.handle)
+}
+
+type HeTransaction struct {
+	handle C.he_t
+}
+
+func (ds *HeDatastore) NewTransaction() (*HeTransaction, error) {
+	logger.Debugf("Creating new transaction")
+	txHandle := C.he_transaction(ds.handle)
+
+	if txHandle == nil {
+		return nil, errors.New("Failed to create new transaction")
+	}
+
+	return &HeTransaction{txHandle}, nil
+}
+
+func (tx *HeTransaction) Put(key []byte, val []byte) error {
+	logger.Debugf("tx.Put called; key size: %d, val size: %d", len(key), len(val))
+	c_key := C.CBytes(key)
+	c_val := C.CBytes(val)
+	key_len := C.size_t(len(key))
+	val_len := C.size_t(len(val))
+
+	defer C.free(c_key)
+	defer C.free(c_val)
+
+	item := C.struct_he_item{c_key, c_val, key_len, val_len}
+
+	ret := C.he_update(tx.handle, &item)
+
+	if int(ret) != 0 {
+		return errors.New("Failed to add item put to transaction")
+	}
+
+	return nil
+}
+
+func (tx *HeTransaction) Delete(key []byte) error {
+	logger.Debugf("tx.Delete called; key size: %d", len(key))
+	c_key := C.CBytes(key)
+	key_len := C.size_t(len(key))
+
+	defer C.free(c_key)
+
+	item := C.struct_he_item{key: c_key, key_len: key_len}
+
+	ret := C.he_delete(tx.handle, &item)
+
+	if int(ret) != 0 {
+		return errors.New("Failed to add item deletion to transaction")
+	}
+
+	return nil
+}
+
+func (tx *HeTransaction) Discard() error {
+	logger.Debugf("tx.Discard called")
+	ret := C.he_discard(tx.handle)
+
+	if int(ret) != 0 {
+		return errors.New("Failed to discard transaction")
+	}
+
+	return nil
+}
+
+func (tx *HeTransaction) Commit() error {
+	logger.Debugf("tx.Commit called")
+	ret := C.he_commit(tx.handle)
+
+	if int(ret) != 0 {
+		return errors.New("Failed to commit transaction")
+	}
+
+	return nil
+}
+
+type HeIterator struct {
+	endKey []byte
+	handle C.he_iter_t
+}
+
+// Get iterator that iterates through range [startKey, endKey)
+func (ds *HeDatastore) GetIterator(startKey []byte, endKey []byte) (*HeIterator, error) {
+	log.Debugf("GetIterator called")
+	c_start_key := C.CBytes(startKey)
+	start_key_len := C.size_t(len(startKey))
+
+	defer C.free(c_start_key)
+
+	seekItem := C.struct_he_item{key: c_start_key, key_len: start_key_len}
+
+	iter := C.he_iter_open(ds.handle, &seekItem, C.size_t(0), C.size_t(HE_MAX_VAL_LEN), C.int(0))
+
+	if iter == nil {
+		return nil, errors.New("Failed to create iterator")
+	}
+
+	return &HeIterator{endKey, iter}, nil
+}
+
+func (iter *HeIterator) Next() ([]byte, []byte) {
+	log.Debugf("iter.Next called")
+	item := C.he_iter_next(iter.handle)
+
+	key := C.GoBytes(item.key, C.int(item.key_len))
+
+	if bytes.Compare(key, iter.endKey) >= 0 {
+		return nil, nil
+	}
+
+	return key, C.GoBytes(item.val, C.int(item.val_len))
+}
+
+func (iter *HeIterator) Close() {
+	log.Debugf("iter.Close called")
+	C.he_iter_close(iter.handle)
+}
---
+++ core/ledger/kvledger/txmgmt/statedb/statehelium/statehelium.go
@@ -0,0 +1,202 @@
+package statehelium
+
+import (
+	"github.com/hyperledger/fabric/common/flogging"
+	"github.com/hyperledger/fabtic/common/ledger/util/heliumhelper"
+	"github.com/hyperledger/fabric/core/ledger/kvledger/txmgmt/statedb"
+)
+
+var logger = flogging.MustGetLogger("statehelium")
+
+var compositeKeySep = []byte{0x00}
+var lastKeyIndicator = byte(0x01)
+var savePointKey = []byte{0x00}
+
+// VersionedDBProvider implements interface VersionedDBProvider
+type VersionedDBProvider struct {
+	dbProvider *heliumhelper.Provider
+}
+
+// NewVersionedDBProvider instantiates VersionedDBProvider
+func NewVersionedDBProvider() *VersionedDBProvider {
+	logger.Debugf("NewVersionedDBProvider called")
+	heUrl := "he://.//dev/nvme0n1" // TODO: Put this in configs
+	logger.Debugf("constructing VersionedDBProvider heUrl=%s", heUrl)
+	dbProvider := heliumhelper.NewProvider(heUrl)
+	return &VersionedDBProvider{dbProvider}
+}
+
+// GetDBHandle gets the handle to a named database
+func (provider *VersionedDBProvider) GetDBHandle(dbName string) (statedb.VersionedDB, error) {
+	logger.Debugf("GetDBHandle called; dbName: %s", dbName)
+	return newVersionedDB(provider.dbProvider.GetDSHandle(dbName), dbName), nil
+}
+
+// Close closes the underlying db
+func (provider *VersionedDBProvider) Close() {
+	logger.Debugf("provider.Close called")
+	// Nothing to do (individual VersionedDB instances are closed)
+}
+
+// VersionedDB implements VersionedDB interface
+type VersionedDB struct {
+	ds *heliumhelper.HeDatastore
+	dbName string
+}
+
+// newVersionedDB constructs an instance of VersionedDB
+func newVersionedDB(ds *heliumhelper.HeDatastore, dbName string) *VersionedDB {
+	return &VersionedDB{ds, dbName}
+}
+
+// Open implements method in VersionedDB interface
+func (vdb *VersionedDB) Open() error {
+	logger.Debugf("vdb.Open called")
+	// do nothing because shared db is used
+	return nil
+}
+
+// Close implements method in VersionedDB interface
+func (vdb *VersionedDB) Close() {
+	logger.Debugf("vdb.Close called")
+	vdb.ds.Close()
+}
+
+// BytesKeySuppoted implements method in VersionedDB interface
+func (vdb *VersionedDB) BytesKeySuppoted() bool {
+	return true
+}
+
+// GetState implements method in VersionedDB interface
+func (vdb *VersionedDB) GetState(namespace string, key string) (*statedb.VersionedValue, error) {
+	logger.Debugf("GetState(). ns=%s, key=%s", namespace, key)
+	compositeKey := constructCompositeKey(namespace, key)
+	dbVal, err := vdb.ds.Get(compositeKey)
+	if err != nil {
+		return nil, err
+	}
+	if dbVal == nil {
+		return nil, nil
+	}
+	val, ver := statedb.DecodeValue(dbVal)
+	return &statedb.VersionedValue{Value: val, Version: ver}, nil
+}
+
+// GetVersion implements method in VersionedDB interface
+func (vdb *VersionedDB) GetVersion(namespace string, key string) (*version.Height, error) {
+	versionedValue, err := vdb.GetState(namespace, key)
+	if err != nil {
+		return nil, err
+	}
+	if versionedValue == nil {
+		return nil, nil
+	}
+	return versionedValue.Version, nil
+}
+
+// GetStateMultipleKeys implements method in VersionedDB interface
+func (vdb *VersionedDB) GetStateMultipleKeys(namespace string, keys []string) ([]*statedb.VersionedValue, error) {
+	vals := make([]*statedb.VersionedValue, len(keys))
+	for i, key := range keys {
+		val, err := vdb.GetState(namespace, key)
+		if err != nil {
+			return nil, err
+		}
+		vals[i] = val
+	}
+	return vals, nil
+}
+
+// GetStateRangeScanIterator implements method in VersionedDB interface
+// startKey is inclusive
+// endKey is exclusive
+func (vdb *VersionedDB) GetStateRangeScanIterator(namespace string, startKey string, endKey string) (statedb.ResultsIterator, error) {
+	compositeStartKey := constructCompositeKey(namespace, startKey)
+	compositeEndKey := constructCompositeKey(namespace, endKey)
+	if endKey == "" {
+		// Increment last byte of composteEndKey (last byte of namespace) to iterate through the end of namespace
+		compositeEndKey[len(compositeEndKey)-1] += lastKeyIndicator
+	}
+	dbItr := vdb.ds.GetIterator(compositeStartKey, compositeEndKey)
+	return newResultsIterator(namespace, dbItr), nil
+}
+
+// ExecuteQuery implements method in VersionedDB interface
+func (vdb *VersionedDB) ExecuteQuery(namespace, query string) (statedb.ResultsIterator, error) {
+	return nil, errors.New("ExecuteQuery not supported for helium")
+}
+
+// ApplyUpdates implements method in VersionedDB interface
+func (vdb *VersionedDB) ApplyUpdates(batch *statedb.UpdateBatch, height *version.Height) error {
+	tx := heliumhelper.NewTransaction()
+	namespaces := batch.GetUpdatedNamespaces()
+	for _, ns := range namespaces {
+		updates := batch.GetUpdates(ns)
+		for k, vv := range updates {
+			compositeKey := constructCompositeKey(ns, k)
+			logger.Debugf("Channel [%s]: Applying key(string)=[%s] key(bytes)=[%#v]", vdb.dbName, string(compositeKey), compositeKey)
+
+			if vv.Value == nil {
+				tx.Delete(compositeKey)
+			} else {
+				tx.Put(compositeKey, statedb.EncodeValue(vv.Value, vv.Version))
+			}
+		}
+	}
+	tx.Put(savePointKey, height.ToBytes())
+	// Setting snyc to true as a precaution, false may be an ok optimization after further testing.
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetLatestSavePoint implements method in VersionedDB interface
+func (vdb *VersionedDB) GetLatestSavePoint() (*version.Height, error) {
+	versionBytes, err := vdb.ds.Get(savePointKey)
+	if err != nil {
+		return nil, err
+	}
+	if versionBytes == nil {
+		return nil, nil
+	}
+	version, _ := version.NewHeightFromBytes(versionBytes)
+	return version, nil
+}
+
+func constructCompositeKey(ns string, key string) []byte {
+	return append(append([]byte(ns), compositeKeySep...), []byte(key)...)
+}
+
+func splitCompositeKey(compositeKey []byte) (string, string) {
+	split := bytes.SplitN(compositeKey, compositeKeySep, 2)
+	return string(split[0]), string(split[1])
+}
+
+// ResultsIterator implements interface ResultsIterator
+type ResultsIterator struct {
+	namespace string
+	dbItr     heliumhelper.HeIterator
+}
+
+func newResultsIterator(namespace string, dbItr heliumhelper.HeIterator) *ResultsIterator {
+	return &ResultsIterator{namespace, dbItr}
+}
+
+func (iterator *ResultsIterator) Next() (statedb.QueryResult, error) {
+	dbKey, dbVal := iterator.dbItr.Next()
+	if dbKey == nil {
+		return nil, nil
+	}
+	dbValCopy := make([]byte, len(dbVal))
+	copy(dbValCopy, dbVal)
+	_, key := splitCompositeKey(dbKey)
+	value, version := statedb.DecodeValue(dbValCopy)
+	return &statedb.VersionedKV{
+		CompositeKey:   statedb.CompositeKey{Namespace: iterator.namespace, Key: key},
+		VersionedValue: statedb.VersionedValue{Value: value, Version: version}}, nil
+}
+
+func (iterator *ResultsIterator) Close() {
+	iterator.dbItr.Close()
+}
